2d8711af5485d5471142e42677c6a27cc0817210
alehuo <aleksi.huotala@helsinki.fi>
2018-12-16 17:12:41 +0200

    Switched from Enum to object

--- dist/Permissions.js
+++ dist/Permissions.js
@@ -1,24 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Permission;
-(function (Permission) {
-    Permission[Permission["ALLOW_REMOVE_USER"] = Math.pow(2, 0)] = "ALLOW_REMOVE_USER";
-    Permission[Permission["ALLOW_VIEW_USERS"] = Math.pow(2, 1)] = "ALLOW_VIEW_USERS";
-    Permission[Permission["ALLOW_ADD_REMOVE_KEYS"] = Math.pow(2, 2)] = "ALLOW_ADD_REMOVE_KEYS";
-    Permission[Permission["ALLOW_VIEW_KEYS"] = Math.pow(2, 3)] = "ALLOW_VIEW_KEYS";
-    Permission[Permission["ALLOW_ADD_EDIT_REMOVE_STUDENT_UNIONS"] = Math.pow(2, 4)] = "ALLOW_ADD_EDIT_REMOVE_STUDENT_UNIONS";
-    Permission[Permission["ALLOW_VIEW_STUDENT_UNIONS"] = Math.pow(2, 5)] = "ALLOW_VIEW_STUDENT_UNIONS";
-    Permission[Permission["ALLOW_ADD_EDIT_REMOVE_EVENTS"] = Math.pow(2, 6)] = "ALLOW_ADD_EDIT_REMOVE_EVENTS";
-    Permission[Permission["ALLOW_VIEW_EVENTS"] = Math.pow(2, 7)] = "ALLOW_VIEW_EVENTS";
-    Permission[Permission["ALLOW_ADD_EDIT_REMOVE_RULES"] = Math.pow(2, 8)] = "ALLOW_ADD_EDIT_REMOVE_RULES";
-    Permission[Permission["ALLOW_VIEW_RULES"] = Math.pow(2, 9)] = "ALLOW_VIEW_RULES";
-    Permission[Permission["ALLOW_ADD_EDIT_REMOVE_POSTS"] = Math.pow(2, 10)] = "ALLOW_ADD_EDIT_REMOVE_POSTS";
-    Permission[Permission["ALLOW_VIEW_POSTS"] = Math.pow(2, 11)] = "ALLOW_VIEW_POSTS";
-    Permission[Permission["ALLOW_ADD_EDIT_REMOVE_LOCATIONS"] = Math.pow(2, 12)] = "ALLOW_ADD_EDIT_REMOVE_LOCATIONS";
-    Permission[Permission["ALLOW_VIEW_LOCATIONS"] = Math.pow(2, 13)] = "ALLOW_VIEW_LOCATIONS";
-    Permission[Permission["ALLOW_ADD_EDIT_REMOVE_MESSAGES"] = Math.pow(2, 14)] = "ALLOW_ADD_EDIT_REMOVE_MESSAGES";
-    Permission[Permission["ALLOW_VIEW_MESSAGES"] = Math.pow(2, 15)] = "ALLOW_VIEW_MESSAGES";
-    Permission[Permission["ALLOW_ADD_EDIT_REMOVE_PERMISSIONS"] = Math.pow(2, 16)] = "ALLOW_ADD_EDIT_REMOVE_PERMISSIONS";
-    Permission[Permission["ALLOW_VIEW_PERMISSIONS"] = Math.pow(2, 17)] = "ALLOW_VIEW_PERMISSIONS";
-})(Permission = exports.Permission || (exports.Permission = {}));
+exports.Permission = {
+    ALLOW_REMOVE_USER: Math.pow(2, 0),
+    ALLOW_VIEW_USERS: Math.pow(2, 1),
+    ALLOW_ADD_REMOVE_KEYS: Math.pow(2, 2),
+    ALLOW_VIEW_KEYS: Math.pow(2, 3),
+    ALLOW_ADD_EDIT_REMOVE_STUDENT_UNIONS: Math.pow(2, 4),
+    ALLOW_VIEW_STUDENT_UNIONS: Math.pow(2, 5),
+    ALLOW_ADD_EDIT_REMOVE_EVENTS: Math.pow(2, 6),
+    ALLOW_VIEW_EVENTS: Math.pow(2, 7),
+    ALLOW_ADD_EDIT_REMOVE_RULES: Math.pow(2, 8),
+    ALLOW_VIEW_RULES: Math.pow(2, 9),
+    ALLOW_ADD_EDIT_REMOVE_POSTS: Math.pow(2, 10),
+    ALLOW_VIEW_POSTS: Math.pow(2, 11),
+    ALLOW_ADD_EDIT_REMOVE_LOCATIONS: Math.pow(2, 12),
+    ALLOW_VIEW_LOCATIONS: Math.pow(2, 13),
+    ALLOW_ADD_EDIT_REMOVE_MESSAGES: Math.pow(2, 14),
+    ALLOW_VIEW_MESSAGES: Math.pow(2, 15),
+    ALLOW_ADD_EDIT_REMOVE_PERMISSIONS: Math.pow(2, 16),
+    ALLOW_VIEW_PERMISSIONS: Math.pow(2, 17),
+};
 //# sourceMappingURL=Permissions.js.map
